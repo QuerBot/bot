81ec7487e1648c9afc0fb08f21a5a98bae285ca6
dstN <github@yinside.de>
2022-04-16 19:43:44 +0200

    Fix

--- src/index.js
+++ src/index.js
@@ -4,8 +4,17 @@ import { getMentions, checkFollowers, getUserID, addToList } from './controller'
 
 const data = JSON.parse(fs.readFileSync('src/assets/accounts.json'));
 
-//getMentions(process.env.BOT_ID);
 
-checkFollowers('example', data);
+async function main() {
+	//getMentions(process.env.BOT_ID);
 
-//addToList("example", await getUserID("example"), data); //manual add to list
+	/* Manual Add to List 
+	let userToAdd = 'example';
+	let manualUser = await getUserID(userToAdd);
+	addToList(userToAdd, manualUser, data);
+	*/
+
+	await checkFollowers('example', data);
+}
+
+main();
