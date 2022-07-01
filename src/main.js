27fe86bc815759e702664232643c114338010b4b
dstN <github@yinside.de>
2022-07-01 19:02:18 +0200

    Added workflow

--- src/main.js
+++ src/main.js
@@ -2,12 +2,6 @@ import 'dotenv/config';
 import * as controller from './controller';
 const CronJob = require('cron').CronJob;
 
-async function test() {
-	let mentions = await controller.getMentions(process.env.BOT_ID);
-	await controller.builder(mentions);
-}
-//test();
-
 const job = new CronJob(
 	'*/10 * * * * *',
 	async function () {
